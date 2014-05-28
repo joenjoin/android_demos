5db36faa35403ffa55cccf5f62fb977e188a6e45
youdian <android@youdian.org>
2014-05-28 17:11:12 +0800

    commit

---
+++ src/org/youdian/android_demos/progressbar/ProgressBarActivity.java
@@ -0,0 +1,18 @@
+package org.youdian.android_demos.progressbar;
+
+import org.youdian.android_demos.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProgressBarActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_progressbar_progressbar);
+	}
+	
+
+}
