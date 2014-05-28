1a54b88844e6c85b3c71d1d68ad3aafa529e2377
youdian <android@youdian.org>
2014-05-28 20:46:04 +0800

    service

---
+++ src/org/youdian/android_demos/service/LongRunningService.java
@@ -0,0 +1,72 @@
+package org.youdian.android_demos.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+/*
+ * Service生命周期
+ * Service生命周期可以从两种启动Service的模式开始讲起，分别是context.startService()和context.bindService()。
+*  (1).startService的启动模式下的生命周期：当我们首次使用startService启动一个服务时，系统会实例化一个 Service实例，
+*  依次调用其onCreate和onStartCommand方法，然后进入运行状态，
+*  此后，如果再使用startService启动 服务时，不再创建新的服务对象，系统会自动找到刚才创建的Service实例，
+*  调用其onStart方法；如果我们想要停掉一个服务，可使用 stopService方法，此时onDestroy方法会被调用，
+*  需要注意的是，不管前面使用了多个次startService，只需一次 stopService，即可停掉服务。
+*  (2).bindService启动模式下的生命周期：在这种模式下，当调用者首次使用bindService绑定一个服务时，
+*  系统会实例化一个 Service实例，并一次调用其onCreate方法和onBind方法，然后调用者就可以和服务进行交互了，
+*  此后，如果再次使用 bindService绑定服务，系统不会创建新的Service实例，也不会再调用onBind方法；
+*  如果我们需要解除与这个服务的绑定，可使用 unbindService方法，此时onUnbind方法和onDestroy方法会被调用。
+ */
+public class LongRunningService extends Service {
+	private static final String TAG="LongRuningService";
+	MyBinder binder;
+	class MyBinder extends Binder{
+		
+		public void log(){
+			Log.d(TAG, "long running service is running");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			log();
+		}
+		
+	}
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		if(binder==null)
+			binder=new MyBinder();
+		return binder;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		// TODO Auto-generated method stub
+		return super.onUnbind(intent);
+	}
+	
+
+}
---
+++ src/org/youdian/android_demos/service/MainActivity.java
@@ -0,0 +1,76 @@
+package org.youdian.android_demos.service;
+
+import org.youdian.android_demos.R;
+import org.youdian.android_demos.service.LongRunningService.MyBinder;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+	Button start,stop,bind,unbind;
+	MyBinder mService;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_service_main);
+		start=(Button)findViewById(R.id.start);
+		stop=(Button)findViewById(R.id.stop);
+		bind=(Button)findViewById(R.id.bind);
+		unbind=(Button)findViewById(R.id.unbind);
+		start.setOnClickListener(this);
+		stop.setOnClickListener(this);
+		bind.setOnClickListener(this);
+		unbind.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+		int id=view.getId();
+		Intent intent=new Intent(this,LongRunningService.class);
+		switch(id){
+		case R.id.start:
+			
+			startService(intent);
+			break;
+		case R.id.stop:
+			stopService(intent);
+			break;
+		case R.id.bind:
+			bindService(intent, conn, Context.BIND_AUTO_CREATE);
+			break;
+		case R.id.unbind:
+			unbindService(conn);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private ServiceConnection conn=new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			mService=(MyBinder) service;
+			mService.log();
+			
+		}
+	};
+	
+
+}
