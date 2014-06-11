bbe386a87e6af507f9f0cc401f88f59ed4f56521
youdian <android@youdian.org>
2014-06-11 13:51:56 +0800

    service

--- src/org/youdian/android_demos/service/MessengerClientActivity.java
+++ src/org/youdian/android_demos/service/MessengerClientActivity.java
@@ -1,7 +1,7 @@
 package org.youdian.android_demos.service;
 
+
 import org.youdian.android_demos.R;
-import org.youdian.android_demos.service.AIDLService;
 
 import android.app.Activity;
 import android.content.ComponentName;
@@ -10,17 +10,17 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.os.RemoteException;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-/*
- *  不推荐使用AIDL进行跨进程通信，因为必须考虑线程安全问题 ,推荐使用Messenger
- */
-public class AIDLClientActivity extends Activity implements OnClickListener {
-	Button bind,unbind;
-	AIDLService mService;
+
+public class MessengerClientActivity extends Activity implements OnClickListener {
+	Button bind;
+	boolean bounded=false;
+	Messenger mService=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -28,46 +28,60 @@ public class AIDLClientActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_service_main);
 		bind=(Button)findViewById(R.id.bind);
 		bind.setOnClickListener(this);
-		unbind=(Button)findViewById(R.id.unbind);
-		unbind.setOnClickListener(this);
-	}
-	@Override
-	public void onClick(View v) {
-		switch(v.getId()){
-		case R.id.bind:
-			Intent intent=new Intent();
-			intent.setClass(this, MyAIDLService.class);
-			//bindService(intent, conn, Context.BIND_AUTO_CREATE);
-			startService(intent);
-			Log.d("AIDLClientActivity", Thread.currentThread().getId()+"");
-			
-			break;
-		case R.id.unbind:
-			try {
-				mService.registerTestCall();
-			} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			break;
-		}
-		
 	}
 	
-	ServiceConnection  conn=new ServiceConnection() {
+	
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		bindService(new Intent(this,MyMessengerService.class), conn, Context.BIND_AUTO_CREATE);
+	}
+
+
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		if(bounded){
+			unbindService(conn);
+			bounded=false;
+		}
+	}
+
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if(!bounded)
+			return;
+		Message msg=Message.obtain(null, MyMessengerService.MSG_HELLO);
+		try{
+			mService.send(msg);
+		}catch(RemoteException e){
+			
+			e.printStackTrace();
+		}
+	}
+	
+	private ServiceConnection conn=new ServiceConnection() {
 		
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
 			// TODO Auto-generated method stub
-			
+			mService=null;
+			bounded=false;
 		}
 		
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
 			// TODO Auto-generated method stub
-			mService=AIDLService.Stub.asInterface(service);
+			mService=new Messenger(service);
+			bounded=true;
 		}
 	};
 
-	
 }
---
+++ src/org/youdian/android_demos/service/MyMessengerService.java
@@ -0,0 +1,46 @@
+package org.youdian.android_demos.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
+/*
+ * 用于跨进程通信
+ */
+public class MyMessengerService extends Service {
+	
+	static final int MSG_HELLO=1;
+	class IncomingHandler extends Handler{
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			switch(msg.what){
+			
+				case MSG_HELLO:
+					sayHello();
+					break;
+				default:
+					super.handleMessage(msg);
+			}
+			
+			
+		}
+		
+	}
+	final Messenger mMessenger = new Messenger(new IncomingHandler());
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return mMessenger.getBinder();
+	}
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_SHORT).show();
+	}
+
+}
