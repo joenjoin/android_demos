550eacf6b47a660844d43c4828e922101db4a8bd
youdian <android@youdian.org>
2014-07-05 21:51:58 +0800

    20140705

---
+++ src/org/youdian/android_demos/broadcast/MainActivity.java
@@ -0,0 +1,39 @@
+package org.youdian.android_demos.broadcast;
+
+import org.youdian.android_demos.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_button);
+		Log.d(GlobalReceiver.TAG, "thread in activity="+Thread.currentThread());
+	}
+
+	public void onClick(View v){
+		int id=v.getId();
+		switch(id){
+		case R.id.button1:
+			sendGlobalBroadcast();
+			break;
+		case R.id.button2:
+			break;
+		}
+	}
+	/*
+	 * 如果BroadcastReceiver声明了自定义权限，那么BroadcastReceiver所在应用需要声明<uses-permission/>和<permission/>，发送者需声明<uses-permission/>
+	 */
+	private void sendGlobalBroadcast() {
+		// TODO Auto-generated method stub
+		Intent intent=new Intent(GlobalReceiver.GLOBAL_ACTION);
+		sendBroadcast(intent,"org.youdian.android_demos.GLOBAL_PERMISSION");
+	}
+}
--- src/org/youdian/android_demos/shortcut/ShortCutActivity.java
+++ src/org/youdian/android_demos/shortcut/ShortCutActivity.java
@@ -1,19 +1,46 @@
 package org.youdian.android_demos.shortcut;
 
+import org.youdian.android_demos.MainActivity;
 import org.youdian.android_demos.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
+/*
+ * @author changjian.zhou 
+ * 创建桌面快捷方式
+ * 快捷方式创建后无法更新界面
+ * 快捷方式只能用于启动Activity,无法启动Service
+ */
 public class ShortCutActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setResult(RESULT_CANCELED);
+		createShortCut();
 	}
 	
+	/*
+	 * 利用anroid系统机制创建Shortcut
+	 */
+	private void createShortCut(){
+		Intent intent=new Intent();
+		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "android_demos");
+		intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher));
+		Intent shortcut=new Intent();
+		shortcut.setClass(this, MainActivity.class);
+		shortcut.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcut);
+		setResult(RESULT_OK, intent);
+		finish();
+	}
+	
+	
+	/*
+	 * 利用Launcher程序内部Broadcast创建Shortcut
+	 * 不推荐
+	 */
 	private void createShortCut(String tName) {
 		// 安装的Intent
 		Intent shortcut = new Intent(
@@ -24,8 +51,8 @@ public class ShortCutActivity extends Activity{
 		shortcut.putExtra("duplicate", false);
 		Intent shortcutIntent = new Intent(Intent.ACTION_MAIN);
 		shortcutIntent.putExtra("tName", tName);
-		shortcutIntent.setClassName("com.qqyumidi.shortcutdemo",
-				"com.qqyumidi.shortcutdemo.AppStart");
+		shortcutIntent.setClassName("org.youdian.android_demos",
+				"org.yodian.android_demos.MainActivity");
 		shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
 		shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
@@ -34,5 +61,6 @@ public class ShortCutActivity extends Activity{
 		// 发送广播
 		sendBroadcast(shortcut);
 	}
+	
 
 }
